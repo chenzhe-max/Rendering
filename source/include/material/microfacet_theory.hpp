2ae18be50c2839ee21102c5451aa702f7e071c3d
chenzhe-max <njuchenzhe@gamil.com>
2026-05-23 15:38:17 +0800

    16

---
+++ source/include/material/microfacet_theory.hpp
@@ -0,0 +1,26 @@
+﻿//微表面理论类
+#pragma once
+
+#include <glm/glm.hpp>
+#include "util/rng.hpp"
+
+class MicrofacetTheory {
+    // Smith Models 史密斯模型
+    // GGX Distribution GGX推导而来
+    // Stretching Invariance 满足伸缩不变性
+public:
+    MicrofacetTheory(float alpha_x, float alpha_z);
+    float normalDistribution(const glm::vec3 &microfacet_normal) const;
+    float masking(const glm::vec3 &view_direction, const glm::vec3 &microfacet_normal) const;
+    float heightCorrelatedMaskingShadowing(const glm::vec3 &light_direction, const glm::vec3 &view_direction, const glm::vec3 &microfacet_normal) const;
+
+    float visibleNormalDistribution(const glm::vec3 &view_direction, const glm::vec3 &microfacet_normal) const;
+    glm::vec3 sampleVisibleNormal(const glm::vec3 &view_direction, const RNG &rng) const;
+
+    bool isDeltaDistibution() const;
+private:
+    float slopeDistribution(const glm::vec2 &slope) const;
+    float Lambda(const glm::vec3 &direction_upper) const;
+private:
+    float alpha_x {}, alpha_z {};
+};
